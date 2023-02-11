5a6bc12116a222ff5c787e24d4ef3f6774929e9b
Sanshyashree123 <Msanthiyamsandhya@gmail.com>
2023-02-11 12:53:43 +0530

    Sai

---
+++ src/com/sa/Todayspro.java
@@ -0,0 +1,12 @@
+package com.sa;
+
+public class Todayspro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "shrini";
+		System.out.println(s);
+	
+
+}
+}
